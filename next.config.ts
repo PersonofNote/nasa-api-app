046fc05172f37475d9d2361e3903af42b1622894
Jessica Martin <apersonofnote@gmail.com>
2026-04-14 17:49:08 -0700

    Set up inital SSR page to fetch and display rough images

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+ images: {
+    remotePatterns: [new URL('https://apod.nasa.gov/**')],
+  },
 };
 
 export default nextConfig;
